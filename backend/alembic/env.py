b413e7ac232e7d01d943574fca4707d48012bf1b
Nicolò Calore <niko.calore97@gmail.com>
2026-06-29 23:38:40 +0200

    Fix a bug in alembic congfiguration

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -11,7 +11,10 @@ from app.db.base import Base
 
 config = context.config
 
-config.set_main_option("sqlalchemy.url", settings.async_database_url)
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.async_database_url.replace("%", "%%"),
+)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
