b4cfcc577acfad1205b98774cadefa5d286a8f0e
Nicolò Calore <niko.calore97@gmail.com>
2026-07-06 14:22:18 +0200

    Release v0.1.0

--- frontend/lib/features/settings/tabs/info_tab.dart
+++ frontend/lib/features/settings/tabs/info_tab.dart
@@ -9,7 +9,7 @@ class InfoTab extends StatelessWidget
   Widget build(BuildContext context)
   {
     final int currentYear = DateTime.now().year;
-    const String appVersion = '0.0.9';
+    const String appVersion = '0.1.0';
 
     //DocumentList
     final List<String> documents = [
